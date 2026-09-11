220cc21759066a8a11bda4e535ad8d7c11514293
jasminlei <jasminleinonen@hotmail.com>
2026-09-11 11:55:28 +0300

    exercise 20

---
+++ migrations/20260211_02_create_reading_lists.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('reading_lists', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onDelete: 'CASCADE',
+      },
+      blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'blogs', key: 'id' },
+        onDelete: 'CASCADE',
+      },
+      read: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    })
+
+    await queryInterface.addIndex('reading_lists', ['user_id', 'blog_id'], {
+      unique: true,
+      name: 'reading_lists_user_blog_unique',
+    })
+  },
+
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('reading_lists')
+  },
+}
--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
+const ReadingList = require('./reading_list')
 const User = require('./user')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList, foreignKey: 'user_id' })
+Blog.belongsToMany(User, { through: ReadingList, foreignKey: 'blog_id' })
+
 module.exports = {
   Blog,
+  ReadingList,
   User,
 }
