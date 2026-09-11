ca868c4245ee69a9db802c20f25e7886ea91a055
jasminlei <jasminleinonen@hotmail.com>
2026-09-11 08:39:47 +0300

    exercise 7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,38 +1,64 @@
 const blogsRouter = require('express').Router()
 const { Blog } = require('../models')
 
-blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
-
-blogsRouter.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body)
-  res.json(blog)
-})
-
-blogsRouter.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-
-  if (blog) {
-    await blog.destroy()
-    res.status(204).end()
-  } else {
-    res.status(404).end()
+blogsRouter.get('/', async (req, res, next) => {
+  try {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+  } catch (error) {
+    next(error)
   }
 })
 
-blogsRouter.put('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-
-  if (blog) {
-    blog.likes = req.body.likes
-
-    await blog.save()
+blogsRouter.post('/', async (req, res, next) => {
+  try {
+    const blog = await Blog.create(req.body)
     res.json(blog)
-  } else {
-    res.status(404).end()
+  } catch (error) {
+    next(error)
   }
 })
+
+blogsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+
+    if (blog) {
+      await blog.destroy()
+      res.status(204).end()
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+
+    if (blog) {
+      blog.likes = req.body.likes
+
+      await blog.save()
+      res.json(blog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  res.status(400).json({
+    error: error.message,
+  })
+}
+
+app.use(errorHandler)
 
 module.exports = blogsRouter
